8200db5c8c9ee8d0243a079c08726b085d99bff0
Алексей Сосин <alekseyvit@mail.ru>
2022-03-17 16:35:37 +0300

    Add main.cpp (Git didn't download it by default?)

---
+++ Client_Server/Client/main.cpp
@@ -0,0 +1,12 @@
+#include "Client.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Client w;
+    //w.runConnection();
+    //w.showImage();
+    w.show();
+    return a.exec();
+}
